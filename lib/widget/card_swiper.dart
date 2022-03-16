8ad1863e6a9bb7c05769916804788747f42dc444
JoseVega <jossvega@gmail.com>
2022-03-15 23:27:04 -0500

    card on

--- lib/widget/card_swiper.dart
+++ lib/widget/card_swiper.dart
@@ -12,19 +12,26 @@ class CardSwiper extends StatelessWidget {
       width: double.infinity,
       height: size.height * 0.3,
       color: Colors.red,
+
+
+
       child: Swiper(
         itemCount: 10,
         layout: SwiperLayout.STACK,
-        itemWidth: size.width * 0.2,
-        itemHeight: size.height * 0.2,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.8,
         pagination: const SwiperPagination(),
         control: const SwiperControl(),
         itemBuilder: (BuildContext context, int index) {
           return const Image(
-            image: NetworkImage('https//via.placeholder.com/300x400')
+            image: NetworkImage('https://cronicaglobal.elespanol.com/uploads/s1/61/11/50/7/main-700b9bff30.jpeg')
             );
         },
       ),
+
+
+
+      
     );
   }
 }
